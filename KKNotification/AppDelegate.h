602be0f13d9d4dd42d3b9985a6c07f658afe8d55
Keje <evekejie@qq.com>
2017-04-05 22:28:53 +0800

    Initial Commit

---
+++ KKNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKNotification
+//
+//  Created by Keje on 2017/4/5.
+//  Copyright © 2017年 Keje. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKNotification/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KKNotification
+//
+//  Created by Keje on 2017/4/5.
+//  Copyright © 2017年 Keje. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
